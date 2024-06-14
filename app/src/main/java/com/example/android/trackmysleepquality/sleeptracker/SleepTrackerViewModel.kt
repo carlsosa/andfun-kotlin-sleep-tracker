1ff58563b0b6f2461a188e4fa7e6f8c0c2c1dbec
Diraj H S <dirajhsdj@gmail.com>
2024-06-14 11:42:35 +0530

    Fix Transformations error
    Signed-off-by: Diraj H S <dirajhsdj@gmail.com>

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -86,7 +86,7 @@ class SleepTrackerViewModel(
      * This is private because we don't want to expose setting this value to the Fragment.
      */
 
-    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight?>()
     /**
      * Call this immediately after calling `show()` on a toast.
      *
@@ -100,7 +100,7 @@ class SleepTrackerViewModel(
     /**
      * If this is non-null, immediately navigate to [SleepQualityFragment] and call [doneNavigating]
      */
-    val navigateToSleepQuality: LiveData<SleepNight>
+    val navigateToSleepQuality: LiveData<SleepNight?>
         get() = _navigateToSleepQuality
 
     /**
